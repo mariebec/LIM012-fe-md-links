cf7f824ab4f7718353d41e306d7aab4646970fe5
mariebec <mariela.benitezc9@gmail.com>
2020-06-01 13:10:34 -0500

    Se convierte a ruta absoluta
    Para que los tests funcionen en cualquier computadora se toma una ruta relativa del proyecto y se convierte a absoluta para usar en los tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,19 +1,21 @@
+const path = require('path');
+
 const {
   absolutePath, pathIsFile, pathIsDirectory, validatePath,
 } = require('../src/index');
 
+const testPath = path.resolve('./src/');
+
 describe('absolutePath', () => {
   it('Debería retornar una ruta absoluta', () => {
-    // Depende de la computadora donde se ejecute el test
-    const path = '/home/dell/Documentos/bootcamp-Lab/LIM012-fe-md-links/src';
-    expect(absolutePath('./src/')).toBe(path);
-    expect(absolutePath(path)).toBe(path);
+    expect(absolutePath('./src/')).toBe(testPath);
+    expect(absolutePath(testPath)).toBe(testPath);
   });
 });
 
 describe('validatePath', () => {
   it('Debería retornar "true" si la ruta es válida', () => {
-    expect(validatePath('./src/')).toBe(true);
+    expect(validatePath(testPath)).toBe(true);
     // expect(validatePath('ffhg')).toBe(false);
   });
 });
@@ -26,6 +28,6 @@ describe('pathIsFile', () => {
 
 describe('pathIsDirectory', () => {
   it('Debería retornar "true" si la ruta corresponde a un directorio', () => {
-    expect(pathIsDirectory('./src/')).toBe(true);
+    expect(pathIsDirectory(testPath)).toBe(true);
   });
 });
