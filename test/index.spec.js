eb4148f22fa2522052d73cace84273aafa643cb1
mariebec <mariela.benitezc9@gmail.com>
2020-06-01 15:17:56 -0500

    Agregando posibles tests
    Se agregaron tests de algunas funciones como mdLinksApi, isMarkdown, allLinks

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,20 @@
 const path = require('path');
 
 const {
-  absolutePath, pathIsFile, pathIsDirectory, validatePath,
+  absolutePath, pathIsFile, pathIsDirectory, validatePath, /* isMarkdown, allLinks, mdLinksApi, */
 } = require('../src/index');
 
 const testPath = path.resolve('./src/');
+// const mdPath = path.resolve('./README.md');
+
+// describe('mdLinksApi', () => {
+// eslint-disable-next-line max-len
+//   it('Debería retornar una promesa con un array en resolve', (done) => mdLinksApi(mdPath, { validate: true })
+//     .then(() => {
+//       expect(mdPath).toEqual(validArray);
+//       done();
+//     }));
+// });
 
 describe('absolutePath', () => {
   it('Debería retornar una ruta absoluta', () => {
@@ -21,7 +31,7 @@ describe('validatePath', () => {
 });
 
 describe('pathIsFile', () => {
-  it('Debería retornar "false" si la ruta no corresponde a un archivo', () => {
+  it('Debería retornar "true" si la ruta corresponde a un archivo', () => {
     expect(pathIsFile('./src/index.js')).toBe(true);
   });
 });
@@ -31,3 +41,17 @@ describe('pathIsDirectory', () => {
     expect(pathIsDirectory(testPath)).toBe(true);
   });
 });
+
+// describe('isMarkdown', () => {
+//   it('Debería retornar "true" si el archivo es .md', () => {
+//     expect(isMarkdown(mdPath)).toBe(true);
+//   });
+// });
+
+// describe('allLinks', () => {
+// eslint-disable-next-line max-len
+//   it('Debería retornar un array con los links encontrados o un array vacío si no hay links', () => {
+//     expect(allLinks(mdPath)).toEqual(testArray);
+//     expect(allLinks(mdEmpty)).toEqual([]);
+//   });
+// });
