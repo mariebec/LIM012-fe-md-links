42e7f01c195884e29a187273caaf2177084da72e
mariebec <mariela.benitezc9@gmail.com>
2020-06-01 11:42:02 -0500

    Test validatePath

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,25 +1,26 @@
 const {
-  absolutePath, pathIsFile, pathIsDirectory,
+  absolutePath, pathIsFile, pathIsDirectory, validatePath,
 } = require('../src/index');
 
 describe('absolutePath', () => {
   it('Debería retornar una ruta absoluta', () => {
-    // Depende de la computadora del que ejecute el test
+    // Depende de la computadora donde se ejecute el test
     const path = '/home/dell/Documentos/bootcamp-Lab/LIM012-fe-md-links/src';
     expect(absolutePath('./src/')).toBe(path);
     expect(absolutePath(path)).toBe(path);
   });
 });
 
-// describe('validatePath', () => {
-//   it('Debería retornar "true" si la ruta es válida', () => {
-//     expect(validatePath('./src/')).toBe(true);
-//   });
-// });
+describe('validatePath', () => {
+  it('Debería retornar "true" si la ruta es válida', () => {
+    expect(validatePath('./src/')).toBe(true);
+    // expect(validatePath('ffhg')).toBe(false);
+  });
+});
 
 describe('pathIsFile', () => {
   it('Debería retornar "false" si la ruta no corresponde a un archivo', () => {
-    expect(pathIsFile('./src/')).toBe(false);
+    expect(pathIsFile('./src/index.js')).toBe(true);
   });
 });
 
