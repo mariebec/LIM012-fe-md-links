1346d13d1bc631dc6cfe0f6bc938d1aa62aa00bd
mariebec <mariela.benitezc9@gmail.com>
2020-06-12 11:57:08 -0500

    Restructuracion de funcion
    Se elimino getMdFiles de esta funcion, tampoco se pregunta si el array de objetos tiene elementos se hara en el cli

--- src/mdlinks.js
+++ src/mdlinks.js
@@ -4,27 +4,23 @@ const mdLinks = (userPath, options) => new Promise((resolve, reject) => {
   if (utilFunctions.validatePath(userPath)) {
     const absPath = utilFunctions.absolutePath(userPath);
 
-    const arrOfLinks = [];
+    let arrOfLinks = [];
     if (utilFunctions.pathIsFile(absPath) && !utilFunctions.getExtension(absPath)) {
       reject(new Error('No se encontró archivos markdown'));
+    } else if (options.validate) {
+      arrOfLinks = utilFunctions.getStatus(absPath);
+      resolve(arrOfLinks);
     } else {
-      utilFunctions.getMdFiles(absPath).forEach((element) => {
-        if (options.validate) {
-          arrOfLinks.push(...utilFunctions.getStatus(element));
-        } else {
-          arrOfLinks.push(...utilFunctions.getLinks(element));
-        }
-      });
+      arrOfLinks.push(...utilFunctions.getLinks(absPath));
+      resolve(arrOfLinks);
     }
-    if (arrOfLinks.length > 0) resolve(Promise.all(arrOfLinks));
-    else reject(new Error('No se encontró archivos markdown con links'));
   } else {
     reject(new Error('Ruta no válida'));
   }
 });
 
 
-// mdLinks('./folder/anotherFolder/README.md', { validate: true }).then((res) => {
+// mdLinks('./folder', { validate: true }).then((res) => {
 //   console.log(res);
 // }).catch((err) => {
 //   console.log(err.message);
